a4a2f854672e2ba3c670667f1a2dd455065c84b1
Rasmus Berggrén <rajje@mac.se>
2015-02-10 10:48:46 +0100

    f

---
+++ js/view/home.js
@@ -0,0 +1,10 @@
+
+
+$("#create-new-dinner").click(createNewDinner);
+// $("body").css("background-image", "url('images/pigtram.jpg')");
+
+function createNewDinner() {
+	$('body').empty();
+	$('body').css("background-image", "none");
+	header();
+}
