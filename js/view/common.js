261a08bba0682f65831c5e1aca2b5c08f2a157a4
Rasmus Berggrén <rajje@mac.se>
2015-02-10 10:31:46 +0100

    Typsnitt

---
+++ js/view/common.js
@@ -0,0 +1,6 @@
+function header() {
+	$("body").append('<div class="container-fluid" id="header">' + 
+		'<span class="title">Homelette</span>' + 
+		'<span class="subtitle">From the best chefs in the world directly into your kitchen</span>' + 
+		'</div>');
+}
